0e14b0ed8a48864abd0508c42143e7b922ce2891
sebastiankrallinger <sebastian.krallinger@htl-saalfelden.at>
2025-02-06 11:58:04 +0100

    Update voucher.controller.ts

--- src/voucher/voucher.controller.ts
+++ src/voucher/voucher.controller.ts
@@ -10,4 +10,28 @@ export class VoucherController {
   async findAll(): Promise<Voucher[]> {
     return this.voucherService.findAll();
   }
+
+  @Get('/count')
+  async getTotalUploads(): Promise<number> {
+    const count = await this.voucherService.findAll();
+    return count.length;
+  }
+
+  @Get('/available')
+  async getActiveWorkspaces(): Promise<number> {
+    const vouchers = await this.voucherService.findAll();
+    const availableVouchers = vouchers.filter(
+      (v) => v.validatedOn > new Date(),
+    );
+    return availableVouchers.length;
+  }
+
+  @Get('/cashed')
+  async getInactiveWorkspaces(): Promise<number> {
+    const vouchers = await this.voucherService.findAll();
+    const availableVouchers = vouchers.filter(
+      (v) => v.validatedOn < new Date(),
+    );
+    return availableVouchers.length;
+  }
 }
